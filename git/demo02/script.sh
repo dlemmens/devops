96f1fb85fb84d1fd9c1e8c71edac2c9af1309bb4
Domien Lemmens <lemmensdomien@hotmail.be>
2024-09-28 09:44:05 +0200

    change na aanmaken bugfix branch

--- git/demo02/script.sh
+++ git/demo02/script.sh
@@ -1,3 +1,4 @@
 # Testen hoe een fetch werkt door online nieuwe file toe te voegen
 echo "Dit is een shell script"
-echo "Dit is een extra regel"
+echo "Dit is een extra regel ____"
+echo "Dit is van de bugfix"
